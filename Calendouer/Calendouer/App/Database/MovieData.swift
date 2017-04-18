731b47ffd09e051d2aee4ce5e78d2de1eafa5364
Desgard_Duan <desgard_duan@foxmail.com>
2017-04-18 13:51:13 +0800

    Complete launch animation closed #6

---
+++ Calendouer/Calendouer/App/Database/MovieData.swift
@@ -0,0 +1,19 @@
+//
+//  MovieData.swift
+//  Calendouer
+//
+//  Created by 段昊宇 on 2017/4/18.
+//  Copyright © 2017年 Desgard_Duan. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class MovieData: Object {
+    
+// Specify properties to ignore (Realm won't persist these)
+    
+//  override static func ignoredProperties() -> [String] {
+//    return []
+//  }
+}
---
+++ Calendouer/Calendouer/App/View/LaunchAnimationView.swift
@@ -0,0 +1,21 @@
+//
+//  LaunchAnimationView.swift
+//  Calendouer
+//
+//  Created by 段昊宇 on 2017/4/18.
+//  Copyright © 2017年 Desgard_Duan. All rights reserved.
+//
+
+import UIKit
+
+class LaunchAnimationView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
